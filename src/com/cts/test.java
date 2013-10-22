6a1d7bb1f60bd9f402a033b2939657d688b1a541
ganesan-sree <390607@PC170510.cts.com>
2013-10-22 18:28:23 +0530

    linkedlist

---
+++ src/com/cts/test.java
@@ -0,0 +1,22 @@
+package com.cts;
+
+class a{
+	
+}
+
+class b {
+	
+}
+
+public class test extends b{
+
+	
+	
+	public static void main(String a[]) {
+		
+		test t = new test();
+		b aa=t;
+		
+		
+	}
+}
