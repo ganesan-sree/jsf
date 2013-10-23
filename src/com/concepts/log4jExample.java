8d5018e5a32dc298777bd5ef56374550c0802bdd
ganesan-sree <390607@PC170510.cts.com>
2013-10-23 19:53:34 +0530

    log

---
+++ src/com/concepts/log4jExample.java
@@ -0,0 +1,26 @@
+package com.concepts;
+
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.sql.SQLException;
+import java.util.*;
+
+public class log4jExample{
+  /* Get actual class name to be printed on */
+  static Logger log = Logger.getLogger(
+                      log4jExample.class);
+
+  public static void main(String[] args)
+                throws IOException,SQLException{
+   
+	  int i=0;
+	  log.trace("Trace Message!");
+      log.debug("Debug Message!");
+      log.info("Info Message!"+i++);
+      log.warn("Warn Message!"+i);
+      log.error("Error Message!");
+      log.fatal("Fatal Message!");
+     System.out.println("1111");
+  }
+}
